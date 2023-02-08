1d3f33fd1986a8c034cf11b80a003e2d06537c50
crockalet <isse_1997@hotmail.com>
2023-02-08 23:49:38 +0500

    feat: search bar component

--- react-app/src/Home.tsx
+++ react-app/src/Home.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import { SearchBar } from "./components/SearchBar";
 
 function Home() {
-  return <div></div>;
+  return (
+    <main className="max-w-screen-md mx-auto">
+      <SearchBar />
+    </main>
+  );
 }
 
 export default Home;
--- react-app/tailwind.config.cjs
+++ react-app/tailwind.config.cjs
@@ -2,6 +2,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      gray: {
+        100: "#F4F4F4",
+        200: "#E9E9E9",
+        300: "#757575",
+        400: "#3A3A3A",
+        500: "#2D2D2D",
+        600: "#1F1F1F",
+        700: "#050505",
+      },
+      white: "#FFFFFF",
+      primary: "#2D2D2D",
+      danger: "#838383",
+    },
     extend: {},
   },
   plugins: [],
